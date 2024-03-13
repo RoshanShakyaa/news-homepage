f11895e0ee021b5525fef09e17ba3c314bfb6476
Roshan Shakya <roshan.shakyaa2015@gmail.com>
2024-03-13 21:37:49 +0545

    initial commit -project complete

---
+++ scripit.js
@@ -0,0 +1,10 @@
+const navbar = document.querySelector("nav");
+const openMenu = document.querySelector("#menu-btn");
+const closeMenu = document.querySelector("#menu-close");
+
+openMenu.addEventListener("click", () => {
+  navbar.classList.add("open");
+});
+closeMenu.addEventListener("click", () => {
+  navbar.classList.remove("open");
+});
